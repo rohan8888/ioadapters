0ea80553028c2737bdf790e1a6bd7c000726d27a
Rohan awad <rohan@dream11.com>
2018-09-25 13:23:10 +0530

    test(test): test cases
    added test cases for util

---
+++ test/utils/util.js
@@ -0,0 +1,16 @@
+const u = require('../../src/utils/util');
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+
+describe('Utility functions test', function() {
+	it('should return true for missing parameters and false for valid parameters', function(done) {
+		let mandatoryParams = ['p1', 'p2'];
+		let input1 = { p1: 'test' },
+			input2 = { p1: 1, p2: 2 };
+		let result1 = u.invalidParams(mandatoryParams, input1);
+		let result2 = u.invalidParams(mandatoryParams, input2);
+		expect(result1).to.be.ok;
+		expect(result2).to.not.be.ok;
+		done();
+	});
+});
