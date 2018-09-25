12b1cbf593cf79ad4574e30c292e5d1624da1150
Rohan awad <rohan@dream11.com>
2018-09-25 17:00:08 +0530

    test

--- test/utils/util.js
+++ test/utils/util.js
@@ -7,8 +7,10 @@ describe('Utility functions test', function() {
 		let mandatoryParams = ['p1', 'p2'];
 		let input1 = { p1: 'test' },
 			input2 = { p1: 1, p2: 2 };
+
 		let result1 = u.invalidParams(mandatoryParams, input1);
 		let result2 = u.invalidParams(mandatoryParams, input2);
+
 		expect(result1).to.be.ok;
 		expect(result2).to.not.be.ok;
 		done();
