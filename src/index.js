c1e61a7b6c219f5165aa2f68472d698183b2b6a4
Rohan awad <rohan@dream11.com>
2018-09-26 20:39:30 +0530

    feat(app): new methods
    redis set and del

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ const { mysql, redis } = require('./db');
 mysql.master('select ?', [1]).then(console.log);
 
 redis.get('default', { key: 'test' }).then(console.log);
+
+redis.set('default', { key: 'test', value: 'text data', expiry: 3600 });
+
+redis.del('default', 'test');
